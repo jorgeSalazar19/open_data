79f0b5c23be1ea7984c3eb952c38fad2c538077f
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2018-10-01 19:02:50 -0500

    push heroku

--- open_data/settings.py
+++ open_data/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,6 +83,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
